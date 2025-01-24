9ba18a0ffc937b786e90a1c764722d11991b4c55
yrolland <yann.rolland@iadvize.com>
2025-01-24 22:22:54 +0100

    wip animation

--- main.go
+++ main.go
@@ -309,7 +309,7 @@ func drawGrid(gtx layout.Context) {
 
 	// linear interpolation
 	destroyedSizePct := lerpRange(1, 0, 0, float64(ANIMATION_SLEEP_MS)/2, float64(time.Since(destroyingSince).Milliseconds()))
-	destroyedSizePct := math.Max(0, destroyedSizePct)
+	destroyedSizePct = math.Max(0, destroyedSizePct)
 
 	for i := 0; i < gameState.Board.Height; i++ {
 		for j := 0; j < gameState.Board.Width; j++ {
