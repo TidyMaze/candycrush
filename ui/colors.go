786648a74808225fe0383bd3f956f605bb6730b0
yrolland <yann.rolland@iadvize.com>
2025-01-25 23:52:01 +0100

    clean

---
+++ ui/colors.go
@@ -0,0 +1,24 @@
+package ui
+
+import "image/color"
+
+var ccBackgroundColor = color.NRGBA{R: 45, G: 109, B: 162, A: 255}
+var emptyColor = color.NRGBA{R: 0, G: 0, B: 0, A: 0}
+var whiteColor = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
+var redColor = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
+var darkRedColor = color.NRGBA{R: 127, G: 0, B: 0, A: 255}
+var yellowColor = color.NRGBA{R: 255, G: 255, B: 0, A: 255}
+var greenColor = color.NRGBA{R: 0, G: 220, B: 0, A: 255}
+var darkGreenColor = color.NRGBA{R: 0, G: 127, B: 0, A: 255}
+var blueColor = color.NRGBA{R: 0, G: 0, B: 255, A: 255}
+var darkBlueColor = color.NRGBA{R: 0, G: 0, B: 127, A: 255}
+var purpleColor = color.NRGBA{R: 150, G: 0, B: 150, A: 255}
+var darkPurpleColor = color.NRGBA{R: 75, G: 0, B: 75, A: 255}
+var orangeColor = color.NRGBA{R: 255, G: 165, B: 0, A: 255}
+var maroon = color.NRGBA{R: 127, G: 0, B: 0, A: 255}
+var slightDark = color.NRGBA{R: 0, G: 0, B: 0, A: 127}
+
+var slightGreen = color.NRGBA{R: 0, G: 255, B: 0, A: 127}
+var slightBlue = color.NRGBA{R: 0, G: 0, B: 255, A: 127}
+var slightRed = color.NRGBA{R: 255, G: 0, B: 0, A: 127}
+var slightOrange = color.NRGBA{R: 255, G: 165, B: 0, A: 127}
--- ui/ui.go
+++ ui/ui.go
@@ -548,27 +548,6 @@ func drawRect(gtx layout.Context, x, y, width, height int, color color.NRGBA) {
 	paint.Fill(gtx.Ops, color)
 }
 
-var ccBackgroundColor = color.NRGBA{R: 45, G: 109, B: 162, A: 255}
-var emptyColor = color.NRGBA{R: 0, G: 0, B: 0, A: 0}
-var whiteColor = color.NRGBA{R: 255, G: 255, B: 255, A: 255}
-var redColor = color.NRGBA{R: 255, G: 0, B: 0, A: 255}
-var darkRedColor = color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-var yellowColor = color.NRGBA{R: 255, G: 255, B: 0, A: 255}
-var greenColor = color.NRGBA{R: 0, G: 220, B: 0, A: 255}
-var darkGreenColor = color.NRGBA{R: 0, G: 127, B: 0, A: 255}
-var blueColor = color.NRGBA{R: 0, G: 0, B: 255, A: 255}
-var darkBlueColor = color.NRGBA{R: 0, G: 0, B: 127, A: 255}
-var purpleColor = color.NRGBA{R: 150, G: 0, B: 150, A: 255}
-var darkPurpleColor = color.NRGBA{R: 75, G: 0, B: 75, A: 255}
-var orangeColor = color.NRGBA{R: 255, G: 165, B: 0, A: 255}
-var maroon = color.NRGBA{R: 127, G: 0, B: 0, A: 255}
-var slightDark = color.NRGBA{R: 0, G: 0, B: 0, A: 127}
-
-var slightGreen = color.NRGBA{R: 0, G: 255, B: 0, A: 127}
-var slightBlue = color.NRGBA{R: 0, G: 0, B: 255, A: 127}
-var slightRed = color.NRGBA{R: 255, G: 0, B: 0, A: 127}
-var slightOrange = color.NRGBA{R: 255, G: 165, B: 0, A: 127}
-
 func getColor(cell engine.Cell) color.NRGBA {
 	switch cell {
 	case engine.Empty:
